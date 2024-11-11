bfcd8405c5d4f5d0df4735f518feed776bdf4ede
Kyle Zarazan <zarazan@no-reply.github.com>
2024-11-10 21:49:46 -0700

    fix tailwind config

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello from React in Rails!</h1>
+      <h1 className="text-3xl text-yellow-700">Hello from React in Rails!</h1>
     </div>
   );
 };
